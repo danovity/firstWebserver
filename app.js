8e4401f0705c891b0247a7f0af97c5a5e3c53dc5
danovity <sean.xiaoxu.ye@gmail.com>
2018-08-07 11:31:04 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const PORT = 8000;
+
+// a function which handles requests and sends response
+function requestHandler(request, response) {
+  response.end(
+    `Requested Path: ${request.url}\nRequest Method: ${request.method}`
+  );
+}
+
+var server = http.createServer(requestHandler);
+
+server.listen(PORT, () => {
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
